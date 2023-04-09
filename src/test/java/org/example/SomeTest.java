a66bc17fd89c1d72e715c9aafe61eb52a544ab57
Roxannnna <roksana2107@gmail.com>
2023-04-09 20:29:02 +0200

    Initial commit

---
+++ src/test/java/org/example/SomeTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeTest {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+}
